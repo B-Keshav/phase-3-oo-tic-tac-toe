6b82e137a9fac8b41279e558e408d21c4bf3d619
Keshav Bhati <keshavbhati2000@gmail.com>
2022-11-02 17:36:54 -0400

    commit tree 2e7192ecdf986a995b557d40c2e716aac9f0bd75

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,56 @@
+class TicTacToe
+    attr_accessor :board, :turn_count
+
+    def initialize
+        @board = [" ", " ", " "," ", " ", " "," ", " ", " "]
+        @turn_count = 0
+    end
+
+    WIN_COMBINATIONS = [ 
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ] 
+
+    def display_board
+       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+       puts "-----------"
+       puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+       puts "-----------"
+       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(input)
+        input.to_i - 1
+    end
+
+    def move(input, token = "X")
+        @board[input] = token
+    end
+
+    def position_taken?(index)
+        @board[index] == "X" || @board[index] == "O" 
+    end
+
+    def valid_move?(index)
+        index.between?(0,8) && !position_taken?(index)
+    end
+
+    def turn_count 
+        @turn_count = @board.filter{|index| index != " "}.length
+    end
+
+    def current_player
+        if @turn_count%2 == 1
+            "O"
+        else
+            "X"
+        end
+    end
+
+end
